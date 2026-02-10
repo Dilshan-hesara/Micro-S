d3d50e081437456718795a799d2a0d371dcb07af
Dilshan Hesara <hesarsdilshan@gmail.com>
2026-02-10 10:37:06 +0530

    Add InventoryController with health and details endpoints

---
+++ inventory-service/src/main/java/com/dilshanhesara/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,21 @@
+package com.dilshanhesara.inventoryservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/inventory")
+public class InventoryController {
+    @GetMapping("/details")
+    public ResponseEntity<?> getDetails() {
+        return new ResponseEntity<>("Inventory service is UP", HttpStatus.OK);
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<?> getHealth() {
+        return new ResponseEntity<>("Health service is UP", HttpStatus.OK);
+    }
+}
