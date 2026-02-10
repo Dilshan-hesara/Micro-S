e7b60046747d76ed8c119afa59e4117bad779829
Dilshan Hesara <hesarsdilshan@gmail.com>
2026-02-10 10:36:25 +0530

    api-service

---
+++ api-service/src/test/java/lk/dilshanhesara/dilshan/apiservice/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.dilshanhesara.dilshan.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
