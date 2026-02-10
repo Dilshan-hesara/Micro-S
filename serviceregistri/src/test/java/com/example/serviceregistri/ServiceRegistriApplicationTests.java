df7705681310d25625de8e4ed18d8edd9b26c94e
Dilshan Hesara <hesarsdilshan@gmail.com>
2026-02-10 10:08:06 +0530

    inventory & service

---
+++ serviceregistri/src/test/java/com/example/serviceregistri/ServiceRegistriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceregistri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
