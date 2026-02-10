5d6cc27af20617a780ffe0089a08892251458c3c
Dilshan Hesara <hesarsdilshan@gmail.com>
2026-02-10 10:24:12 +0530

    serviceregistry

---
+++ inventory-service/src/main/java/edu/ijse/gdse71/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,4 @@
+package edu.ijse.gdse71.inventoryservice.controller;
+
+public class InventoryController {
+}
---
+++ order-service/src/main/java/edu/ijse/gdse71/orderservice/api/controller/OrderController.java
@@ -0,0 +1,4 @@
+package edu.ijse.gdse71.orderservice.api.controller;
+
+public class OrderController {
+}
