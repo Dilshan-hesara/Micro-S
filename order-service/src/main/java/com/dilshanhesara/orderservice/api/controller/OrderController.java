4f236c7147788968d0e1073c9d153ee9c40b1ff5
Dilshan Hesara <hesarsdilshan@gmail.com>
2026-02-16 10:29:05 +0530

    Add inventory service call to order controller and update details response

--- order-service/src/main/java/com/dilshanhesara/orderservice/api/controller/OrderController.java
+++ order-service/src/main/java/com/dilshanhesara/orderservice/api/controller/OrderController.java
@@ -1,21 +1,36 @@
 package com.dilshanhesara.orderservice.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
     @GetMapping("/details")
     public ResponseEntity<?> getDetails() {
-        return new ResponseEntity<>("Order service is UP", HttpStatus.OK);
+        return new ResponseEntity<>("Inventory service is UP", HttpStatus.OK);
     }
 
     @GetMapping("/health")
     public ResponseEntity<?> getHealth() {
         return new ResponseEntity<>("Health service is UP", HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("/call-inventory")
+    public ResponseEntity<?> callInventoryService() {
+        String response = restTemplate.getForObject("http://Inventory-service/call-inventory", String.class);
+        return new ResponseEntity<>("From order successfully"+ response, HttpStatus.OK);
     }
 }
